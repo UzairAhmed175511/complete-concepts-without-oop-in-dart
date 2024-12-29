e449fa4ecece56f6d830755619bb19d57b70877c
UZAIR AHMED <uzairahmed175511@gmail.com>
2024-12-29 09:51:05 +0500

    print all strings in specific format

---
+++ Question (1).dart
@@ -0,0 +1,12 @@
+void main() {
+//                                             //QUESTION (1)
+//        /*Write a Dart program to print the following string in a specific format Sample String : "Twinkle, twinkle,
+//        little star, How I wonder what you are! Up above the world so high, Like a diamond in the sky. Twinkle,
+//        twinkle, little star, How I wonder what you are" Output:*/
+  print('Twinkle, twinkle,little star,');
+  print('How I wonder what you are!');
+  print('Up above the world so high,');
+  print('Like a diamond in the sky.');
+  print('Twinkle,twinkle, little star,');
+  print('How I wonder what you are');
+}
