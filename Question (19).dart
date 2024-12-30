71ef3e5c0368f246363e622fa47ea08273b3b214
UZAIR AHMED <uzairahmed175511@gmail.com>
2024-12-30 13:59:06 +0500

    list of all the divisors of that numbers

---
+++ Question (19).dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+void main() {
+  //QUESTION (20)
+  //Create a program that ask the user for a number and then print out a list of all the divisors of that numbers
+  print("enter the number");
+  int h = int.parse(stdin.readLineSync()!);
+  List<int> emptylist = [];
+  for (int i = 1; i < h; i++) {
+    if (h % i == 0) {
+      emptylist.add(i);
+    }
+  }
+  print(emptylist);
+}
