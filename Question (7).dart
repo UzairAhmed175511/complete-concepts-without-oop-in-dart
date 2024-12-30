6ad417b4becc87149e9da096f03e71a6bd7ffb7c
UZAIR AHMED <uzairahmed175511@gmail.com>
2024-12-30 13:30:34 +0500

    check string it start with (IS or not)

--- Question (7).dart
+++ Question (7).dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 void main() {
+  //QUESTION (7)
   //Write a Dart program to get a new string from a given string where "Is" has been added to the front.
   //If the given string already begins with "Is" then return the string unchanged.
   print('enter the string');
